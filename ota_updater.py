39ee4715adfb89b46d9cd38cb7c987fb77fea4fa
rdehuyss <ronald.dehuysser@gmail.com>
2018-07-18 22:19:45 +0200

    first version

---
+++ ota_updater.py
@@ -0,0 +1,228 @@
+# 1000 x dank aan Evelien die mijn in deze tijden gesteund heeft
+# ohja, en er is ook nog tante suker (Jana Dej.) die graag kinderen wilt maar het zelf nog niet beseft
+
+import usocket
+import os
+
+
+class OTAUpdater:
+
+    def __init__(self, github_repo, main_dir='main'):
+        self.http_client = HttpClient()
+        self.github_repo = github_repo.replace('https://github.com', 'https://api.github.com/repos')
+        self.main_dir = main_dir
+
+    def using_network(self, ssid='WiFi-2.4-62A1', password='internetisvaniedereen'):
+        import network
+        sta_if = network.WLAN(network.STA_IF)
+        if not sta_if.isconnected():
+            print('connecting to network...')
+            sta_if.active(True)
+            sta_if.connect(ssid, password)
+            while not sta_if.isconnected():
+                pass
+        print('network config:', sta_if.ifconfig())
+
+    def apply_pending_updates_if_available(self):
+        if 'next' in os.listdir():
+            print('Pending update found: ', self.get_version('next'))
+            self.rmtree('main')
+            os.rename('next', 'main')
+
+    def download_updates_if_available(self):
+        current_version = self.get_version('main')
+        latest_version = self.get_latest_version()
+
+        print('Checking version... ')
+        print('\tCurrent version: ', current_version)
+        print('\tLatest version: ', latest_version)
+        if latest_version > current_version:
+            print('Updating...')
+            os.mkdir('next')
+            self.download_all_files(self.github_repo + '/contents/' + self.main_dir, latest_version)
+            with open('next/.version', 'w') as versionfile:
+                versionfile.write(latest_version)
+                versionfile.close()
+
+            return True
+        return False
+            
+
+    def rmtree(self, top):
+        for entry in os.ilistdir(top):
+            is_dir = entry[1] == 0x4000
+            if is_dir:
+                self.rmtree(top + '/' + entry[0])
+
+            else:
+                os.remove(top + '/' + entry[0])
+        os.rmdir(top)
+
+    def get_version(self, directory):
+        f = open(directory + '/.version')
+        version = f.read()
+        f.close()
+        return version
+
+    def get_latest_version(self):
+        latest_release = self.http_client.get(self.github_repo + '/releases/latest')
+        version = latest_release.json()['tag_name']
+        latest_release.close()
+        return version
+
+    def download_all_files(self, root_url, version):
+        file_list = self.http_client.get(root_url + '?ref=refs/tags/' + version)
+        for file in file_list.json():
+            try:
+                if file['type'] == 'file':
+                    download_url = file['download_url']
+                    download_path = 'next/' + file['path'].replace('main/', '')
+                    self.download_file(download_url.replace('refs/tags/', ''), download_path)
+                elif file['type'] == 'dir':
+                    path = 'next/' + file['path'].replace('main/', '')
+                    os.mkdir(path)
+                    self.download_all_files(root_url + '/' + file['name'], version)
+            except:
+                print(file)
+        file_list.close()
+
+    def download_file(self, url, path):
+        print('\tDownloading: ', path)
+        with open(path, 'w') as outfile:
+            try:
+                response = self.http_client.get(url)
+                outfile.write(response.text)
+                response.close()
+            finally:
+                response.close()
+                outfile.close()
+
+
+class Response:
+
+    def __init__(self, f):
+        self.raw = f
+        self.encoding = 'utf-8'
+        self._cached = None
+
+    def close(self):
+        if self.raw:
+            self.raw.close()
+            self.raw = None
+        self._cached = None
+
+    @property
+    def content(self):
+        if self._cached is None:
+            try:
+                self._cached = self.raw.read()
+            finally:
+                self.raw.close()
+                self.raw = None
+        return self._cached
+
+    @property
+    def text(self):
+        return str(self.content, self.encoding)
+
+    def json(self):
+        import ujson
+        return ujson.loads(self.content)
+
+
+class HttpClient:
+
+    def request(self, method, url, data=None, json=None, headers={}, stream=None):
+        try:
+            proto, dummy, host, path = url.split('/', 3)
+        except ValueError:
+            proto, dummy, host = url.split('/', 2)
+            path = ''
+        if proto == 'http:':
+            port = 80
+        elif proto == 'https:':
+            import ussl
+            port = 443
+        else:
+            raise ValueError('Unsupported protocol: ' + proto)
+
+        if ':' in host:
+            host, port = host.split(':', 1)
+            port = int(port)
+
+        ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
+        ai = ai[0]
+
+        s = usocket.socket(ai[0], ai[1], ai[2])
+        try:
+            s.connect(ai[-1])
+            if proto == 'https:':
+                s = ussl.wrap_socket(s, server_hostname=host)
+            s.write(b'%s /%s HTTP/1.0\r\n' % (method, path))
+            if not 'Host' in headers:
+                s.write(b'Host: %s\r\n' % host)
+            # Iterate over keys to avoid tuple alloc
+            for k in headers:
+                s.write(k)
+                s.write(b': ')
+                s.write(headers[k])
+                s.write(b'\r\n')
+            # add user agent
+            s.write('User-Agent')
+            s.write(b': ')
+            s.write('MicroPython OTAUpdater')
+            s.write(b'\r\n')
+            if json is not None:
+                assert data is None
+                import ujson
+                data = ujson.dumps(json)
+                s.write(b'Content-Type: application/json\r\n')
+            if data:
+                s.write(b'Content-Length: %d\r\n' % len(data))
+            s.write(b'\r\n')
+            if data:
+                s.write(data)
+
+            l = s.readline()
+            # print(l)
+            l = l.split(None, 2)
+            status = int(l[1])
+            reason = ''
+            if len(l) > 2:
+                reason = l[2].rstrip()
+            while True:
+                l = s.readline()
+                if not l or l == b'\r\n':
+                    break
+                # print(l)
+                if l.startswith(b'Transfer-Encoding:'):
+                    if b'chunked' in l:
+                        raise ValueError('Unsupported ' + l)
+                elif l.startswith(b'Location:') and not 200 <= status <= 299:
+                    raise NotImplementedError('Redirects not yet supported')
+        except OSError:
+            s.close()
+            raise
+
+        resp = Response(s)
+        resp.status_code = status
+        resp.reason = reason
+        return resp
+
+    def head(self, url, **kw):
+        return self.request('HEAD', url, **kw)
+
+    def get(self, url, **kw):
+        return self.request('GET', url, **kw)
+
+    def post(self, url, **kw):
+        return self.request('POST', url, **kw)
+
+    def put(self, url, **kw):
+        return self.request('PUT', url, **kw)
+
+    def patch(self, url, **kw):
+        return self.request('PATCH', url, **kw)
+
+    def delete(self, url, **kw):
+        return self.request('DELETE', url, **kw)
